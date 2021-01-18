2443c863710b0c68565dd36e915d739f9555b569
Maksym Seredenko <max.seredenko@gmail.com>
2021-01-18 15:46:01 +0200

    removed sudo from install

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,6 +1,6 @@
 if [ "$INITIAL_COMMIT_DONE" != "true" ]; then
-    sudo apt update
-    sudo apt -y install python3-virtualenv
+    apt update
+    apt -y install python3-virtualenv
     virtualenv venv
     source venv/bin/activate
     gunicorn app.server:app --bind=0.0.0.0:80 -w 5 -k uvicorn.workers.UvicornWorker --daemon --reload
