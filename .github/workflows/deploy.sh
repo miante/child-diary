a2aecf91effdacff68659fb86927522593184131
Maksym Seredenko <max.seredenko@gmail.com>
2021-01-18 15:44:05 +0200

    updated deployment config

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,12 @@
+if [ "$INITIAL_COMMIT_DONE" != "true" ]; then
+    sudo apt update
+    sudo apt -y install python3-virtualenv
+    virtualenv venv
+    source venv/bin/activate
+    gunicorn app.server:app --bind=0.0.0.0:80 -w 5 -k uvicorn.workers.UvicornWorker --daemon --reload
+    echo "INITIAL_COMMIT_DONE=true" >> ~/.bashrc
+    source ~/.bashrc
+else
+  source venv/bin/activate
+  pip install -U -r requirements.txt
+fi
