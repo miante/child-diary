e0bbc06c3cae4c9a8ccb5a930faf9d7475764ec1
Maksym Seredenko <max.seredenko@gmail.com>
2021-01-19 12:37:09 +0200

    configure file logging

--- app/core/log.py
+++ app/core/log.py
@@ -1,38 +1,43 @@
 import logging.config
 
 
-LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)-6s]  %(message)s',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'formatter': 'standard',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
-}
-
-
 def configure_logging():
     """
     Base configure function for logging withing application
     """
 
-    logging.config.dictConfig(LOGGING_CONFIG)
+    config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)-6s]  %(message)s',
+            },
+        },
+        'handlers': {
+            'console': {
+                'level': 'INFO',
+                'formatter': 'standard',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
+            },
+            'file': {
+                'class': 'logging.FileHandler',
+                'filename': '../child-diary.log',
+                'mode': 'a',
+                'formatter': 'standard',
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['console', 'file'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        }
+    }
+
+    logging.config.dictConfig(config)
 
 
 logger = logging.getLogger('app')
